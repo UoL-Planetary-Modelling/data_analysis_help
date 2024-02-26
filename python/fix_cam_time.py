936f8640197565789416e6b183de3a305332f4bd
Dan Marsh <37961632+dan800@users.noreply.github.com>
2024-02-26 14:33:36 +0000

    Update fix_cam_time.py
    added comment as to use

--- python/fix_cam_time.py
+++ python/fix_cam_time.py
@@ -1,3 +1,8 @@
+# A python function that subtracts 15 days from the date field - use it cautiously! 
+# It can fix the issue in monthly (h0) history files where the date field is the time 
+# the data are written and not the center of the averaging period. 
+# So in h0s the date may be 2001-02-01 but it refers to the January mean.
+
 def fix_cam_time(ds):
 
     from cftime import DatetimeNoLeap
